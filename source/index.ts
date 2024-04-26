290d72315e01a6c91eef2dc5292f23ed73876f67
Christian Ashley <christianzashley@gmail.com>
2024-04-26 16:45:20 -0400

    Add support for lowercase alphanumeric characters
    * Create type BrailleGrade for UEB grades
    * Create interface BrailleState to represent states of braille
    * Create function printToUEB to parse string inputs
    * Create function printWordToUEB to parse individual words
    * Create function printCharToUEB to parse individual characters

--- source/index.ts
+++ source/index.ts
@@ -1,1 +1,66 @@
-console.log("hello world");
+type BrailleGrade = 1 | 2;
+
+interface BrailleState {
+  grade: BrailleGrade;
+  numeric: boolean;
+  capital: boolean;
+}
+
+function printToUEB(s: string) {
+  const braille: string[] = [];
+  let word: string[] = [];
+  const state: BrailleState = {
+    grade: 1,
+    numeric: false,
+    capital: false,
+  };
+
+  for (const c of s) {
+    if (c === " ") {
+      braille.push(printWordToUEB(word, state));
+      word = [];
+      braille.push(" ");
+      state.numeric = false;
+      state.capital = false;
+    } else {
+      word.push(c);
+    }
+  }
+  if (word.length > 0) braille.push(printWordToUEB(word, state));
+  return braille.join("");
+}
+
+function printWordToUEB(w: string[], s: BrailleState) {
+  const braille: string[] = [];
+
+  // TODO: word translation should check for word/group signs and general exceptions
+  // if grade 2 ueb
+  for (const c of w) {
+    braille.push(printCharToUEB(c, s));
+  }
+
+  return braille.join("");
+}
+
+function printCharToUEB(c: string, s: BrailleState): string {
+  const codePoint = c.codePointAt(0);
+  if (codePoint === undefined) throw new Error("input must be a string");
+  if (codePoint >= 48 && codePoint <= 57) {
+    // standard numbers
+    let numberSign = !s.numeric ? "#" : "";
+    s.numeric = true;
+    s.grade = 1;
+    return numberSign + String.fromCodePoint(65 + ((codePoint - 48 + 9) % 10));
+  }
+  if (codePoint === 44 || codePoint === 46) {
+    return codePoint === 44 ? "1" : "4";
+  }
+  if (s.numeric) s.numeric = false;
+  if (codePoint >= 97 && codePoint <= 120) {
+    // lowercase standard alphabet
+    return String.fromCodePoint(codePoint - 32);
+  }
+  return "*";
+}
+
+console.log(printToUEB("hello world 123402"));
